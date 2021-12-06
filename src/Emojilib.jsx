c17947cca139c9fb6734af80d75fe4293bc8bec1
Ericogia <ogia.aghahowa@gmail.com>
2021-12-06 15:44:22 +0100

    More to lib

--- src/Emojilib.jsx
+++ src/Emojilib.jsx
@@ -19,7 +19,31 @@ const Emojilib = [
       name: "Rolling On The Floor, Laughing",
       meaning:
         "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
-    }
+    },
+    {
+        id: 4,
+        emoji: "😆",
+        name: " Smiling Face With Open Mouth And Closed Eyes",
+        meaning:
+          "Emoji is on the verge of a fit of laughter because something is so silly or incredibly funny. Also, mischievous or bitchy laughter about an event, or something that happened to another person.. Means the same as “XD“ and “X“ as a text-based emoticon."
+      },
+      ,
+    {
+        id: 5,
+        emoji: "🙃",
+        name: "Upside-Down Face",
+        meaning:
+          "Don't take me seriously! The message is either ambiguous, ironic or joking. Person plays the fool and horses around, or in response to a joke."
+      },
+      ,
+    {
+        id: 6,
+        emoji: "👐",
+        name: "Open Hands",
+        meaning:
+          "The two opened hands are stretched toward the counterpart. Represents affection and openness. Could also stand for a hug."
+      },
+     
   ];
 
 export default Emojilib;
